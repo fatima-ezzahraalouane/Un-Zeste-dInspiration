a7c7f4225e96317361d2a120752cd878e8918600
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-06 23:45:50 +0100

    UZDI-2 UZDI-3 UZDI-5 UZDI-6 UZDI-7 UZDI-8 UZDI-9 UZDI-22 ajouter des colonnes et un type ENUM pour la table des utilisateurs

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,15 +12,25 @@ return new class extends Migration
      */
     public function up(): void
     {
+        DB::statement("CREATE TYPE statut_enum AS ENUM ('actif', 'inactif', 'suspendu')");
+
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->text('password');
+            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
+            $table->string('statut')->default('actif');
+            $table->boolean('is_approved')->default(false);
+            $table->text('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
+
+        // Convertir la colonne 'statut' en ENUM PostgreSQL
+        DB::statement("ALTER TABLE users ALTER COLUMN statut TYPE statut_enum USING statut::statut_enum");
     }
 
     /**
@@ -28,5 +39,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
+        DB::statement("DROP TYPE IF EXISTS statut_enum");
     }
 };
