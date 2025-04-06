1f1afde2ab84a21bc078201e2d9d72630324097b
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-07 00:46:00 +0100

    UZDI-2 UZDI-3 UZDI-5 UZDI-6 UZDI-7 UZDI-8 UZDI-9 UZDI-22 ajouter la migration pour créer la table roles avec un type ENUM pour les rôles d'utilisateur

---
+++ database/migrations/2014_10_11_000000_create_roles_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement("DROP TYPE IF EXISTS role_user");
+        
+        DB::statement("CREATE TYPE role_user AS ENUM ('Client', 'Chef', 'Admin')");
+
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_user')->unique();
+            $table->timestamps();
+        });
+
+        // Modifier la colonne pour utiliser le type ENUM PostgreSQL
+        DB::statement("ALTER TABLE roles ALTER COLUMN name_user TYPE role_user USING name_user::role_user");
+
+        // Ajouter la contrainte NOT NULL
+        DB::statement("ALTER TABLE roles ALTER COLUMN name_user SET NOT NULL");
+
+        DB::table('roles')->insert([
+            ['name_user' => 'Client'],
+            ['name_user' => 'Chef'],
+            ['name_user' => 'Admin'],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+        DB::statement("DROP TYPE IF EXISTS role_user");    }
+};
