5bc93075713b6ccec387f6c6dc65e6bbb2397164
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-14 15:03:20 +0100

    UZDI-1 UZDI-2 UZDI-3 UZDI-5 UZDI-6 UZDI-7 UZDI-8 UZDI-9 UZDI-22 UZDI-72 UZDI-137 UZDI-147 UZDI-148 UZDI-149 UZDI-150 UZDI-151 UZDI-152 ajouter un seeder pour créer un utilisateur administrateur avec des informations par défaut

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         $adminUser = User::create([
+            'first_name' => 'Fatima-Ezzahra',
+            'last_name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('adminfati123'),
+            'role_id' => 3,
+            'statut' => 'Actif',
+            'is_approved' => true,
+            'avatar' => null,
+            'remember_token' => Str::random(10),
+        ]);
+
+        DB::table('admins')->insert([
+            'user_id' => $adminUser->id,
+            'telephone' => '0600000000',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
