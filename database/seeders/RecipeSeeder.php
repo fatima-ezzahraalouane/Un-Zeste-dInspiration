299b504b407dfbcee24dee4e19c9e851d545f02b
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-21 11:53:07 +0100

    UZDI-72 UZDI-73 UZDI-74 UZDI-75 UZDI-76 UZDI-81 UZDI-82 UZDI-83 UZDI-84 Ajout du seeder RecipeSeeder pour insérer des recettes par défaut dans la base de données.

---
+++ database/seeders/RecipeSeeder.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RecipeSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $recipes = [
+            [
+                'title' => 'Salade Niçoise',
+                'image' => 'salade-nicoise.jpg',
+                'description' => 'Une salade fraîche typique du sud de la France.',
+                'preparation_time' => 15,
+                'cooking_time' => 0,
+                'servings' => 2,
+                'complexity' => 'Facile',
+                'ingredients' => json_encode(['tomates', 'thon', 'œufs', 'haricots verts', 'olives']),
+                'instructions' => json_encode(['Couper les légumes', 'Mélanger avec le thon et les œufs']),
+                'category_id' => 1,
+            ],
+            [
+                'title' => 'Tajine de poulet',
+                'image' => 'tajine-poulet.jpg',
+                'description' => 'Tajine traditionnel au citron confit et olives.',
+                'preparation_time' => 20,
+                'cooking_time' => 60,
+                'servings' => 4,
+                'complexity' => 'Moyen',
+                'ingredients' => json_encode(['poulet', 'citron confit', 'olives', 'épices']),
+                'instructions' => json_encode(['Faire revenir le poulet', 'Ajouter les ingrédients et mijoter']),
+                'category_id' => 2,
+            ],
+            [
+                'title' => 'Couscous royal',
+                'image' => 'couscous.jpg',
+                'description' => 'Plat marocain à base de semoule, légumes et viande.',
+                'preparation_time' => 30,
+                'cooking_time' => 90,
+                'servings' => 6,
+                'complexity' => 'Difficile',
+                'ingredients' => json_encode(['semoule', 'agneau', 'poulet', 'légumes']),
+                'instructions' => json_encode(['Cuire les viandes', 'Préparer les légumes', 'Servir avec la semoule']),
+                'category_id' => 2,
+            ],
+            [
+                'title' => 'Soupe de légumes',
+                'image' => 'soupe-legumes.jpg',
+                'description' => 'Soupe réconfortante à base de légumes frais.',
+                'preparation_time' => 15,
+                'cooking_time' => 30,
+                'servings' => 4,
+                'complexity' => 'Facile',
+                'ingredients' => json_encode(['carottes', 'pommes de terre', 'oignons', 'courgettes']),
+                'instructions' => json_encode(['Faire revenir les légumes', 'Ajouter de l’eau et cuire']),
+                'category_id' => 1,
+            ],
+            [
+                'title' => 'Quiche lorraine',
+                'image' => 'quiche.jpg',
+                'description' => 'Tarte salée aux lardons et à la crème.',
+                'preparation_time' => 20,
+                'cooking_time' => 35,
+                'servings' => 6,
+                'complexity' => 'Moyen',
+                'ingredients' => json_encode(['pâte brisée', 'lardons', 'œufs', 'crème fraîche']),
+                'instructions' => json_encode(['Préparer la garniture', 'Cuire au four']),
+                'category_id' => 2,
+            ],
+            [
+                'title' => 'Gâteau au chocolat',
+                'image' => 'gateau-chocolat.jpg',
+                'description' => 'Gâteau moelleux au chocolat noir.',
+                'preparation_time' => 15,
+                'cooking_time' => 25,
+                'servings' => 8,
+                'complexity' => 'Facile',
+                'ingredients' => json_encode(['chocolat', 'beurre', 'farine', 'œufs', 'sucre']),
+                'instructions' => json_encode(['Faire fondre le chocolat', 'Mélanger les ingrédients', 'Cuire au four']),
+                'category_id' => 3,
+            ],
+            [
+                'title' => 'Crêpes sucrées',
+                'image' => 'crepes.jpg',
+                'description' => 'Crêpes fines à garnir selon vos envies.',
+                'preparation_time' => 10,
+                'cooking_time' => 20,
+                'servings' => 6,
+                'complexity' => 'Facile',
+                'ingredients' => json_encode(['farine', 'lait', 'œufs', 'sucre', 'beurre']),
+                'instructions' => json_encode(['Préparer la pâte', 'Cuire les crêpes dans une poêle']),
+                'category_id' => 3,
+            ],
+            [
+                'title' => 'Tiramisu',
+                'image' => 'tiramisu.jpg',
+                'description' => 'Dessert italien à base de mascarpone et café.',
+                'preparation_time' => 25,
+                'cooking_time' => 0,
+                'servings' => 6,
+                'complexity' => 'Moyen',
+                'ingredients' => json_encode(['mascarpone', 'café', 'biscuits', 'œufs', 'sucre']),
+                'instructions' => json_encode(['Préparer la crème', 'Monter le dessert en couches', 'Réfrigérer']),
+                'category_id' => 3,
+            ],
+            [
+                'title' => 'Gratin dauphinois',
+                'image' => 'gratin.jpg',
+                'description' => 'Plat de pommes de terre à la crème et au fromage.',
+                'preparation_time' => 15,
+                'cooking_time' => 60,
+                'servings' => 4,
+                'complexity' => 'Moyen',
+                'ingredients' => json_encode(['pommes de terre', 'crème', 'fromage râpé', 'ail']),
+                'instructions' => json_encode(['Couper les pommes de terre', 'Cuire au four avec crème et fromage']),
+                'category_id' => 2,
+            ],
+            [
+                'title' => 'Velouté de potiron',
+                'image' => 'veloute.jpg',
+                'description' => 'Soupe douce à base de potiron.',
+                'preparation_time' => 10,
+                'cooking_time' => 30,
+                'servings' => 4,
+                'complexity' => 'Facile',
+                'ingredients' => json_encode(['potiron', 'oignon', 'crème', 'beurre']),
+                'instructions' => json_encode(['Faire revenir les légumes', 'Mixer avec de la crème']),
+                'category_id' => 1,
+            ],
+        ];
+
+        foreach ($recipes as $recipe) {
+            DB::table('recipes')->insert(array_merge($recipe, [
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]));
+        }
+    }
+}
