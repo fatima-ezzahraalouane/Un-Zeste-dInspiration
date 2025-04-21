a59bddfffee1d32c78a7f91507ce92fdbcbc3dd0
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-21 13:51:48 +0100

    UZDI-72 UZDI-73 UZDI-74 UZDI-75 UZDI-76 UZDI-81 UZDI-82 UZDI-83 UZDI-84 Ajout du modèle Category avec une relation pour associer plusieurs recettes.

---
+++ app/Models/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    // 🔗 Relation : Une catégorie a plusieurs recettes
+    public function recipes()
+    {
+        return $this->hasMany(Recipe::class);
+    }
+}
