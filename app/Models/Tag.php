e3efc651b272c64ae436f9b440fa7d15455f2da1
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-21 13:51:22 +0100

    UZDI-72 UZDI-73 UZDI-74 UZDI-75 UZDI-76 UZDI-81 UZDI-82 UZDI-83 UZDI-84 Ajout du modèle Tag avec la relation pour associer des recettes.

---
+++ app/Models/Tag.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    // 🔗 Relation : Un tag peut être associé à plusieurs recettes
+    public function recipes()
+    {
+        return $this->belongsToMany(Recipe::class, 'recipe_tag');
+    }
+}
