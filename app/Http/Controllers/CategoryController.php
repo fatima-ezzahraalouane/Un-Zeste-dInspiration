6fec5241350a82ab191b5ea3b9714adb8800b07d
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-22 14:40:07 +0100

    UZDI-72 UZDI-73 UZDI-74 UZDI-75 UZDI-76 UZDI-81 UZDI-82 UZDI-83 UZDI-84 Ajout du contrôleur CategoryController pour gérer les catégories

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Category\StoreCategoryRequest;
+use App\Http\Requests\Category\UpdateCategoryRequest;
+use App\Repositories\Category\CategoryRepositoryInterface;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    protected CategoryRepositoryInterface $categoryRepository;
+
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    public function index()
+    {
+        $categories = $this->categoryRepository->getAll();
+        return view('categories.index', compact('categories'));
+    }
+}
