4bc0b30dda8f926b375ab084110be0ae64feaa1b
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-22 17:27:23 +0100

    UZDI-72 UZDI-73 UZDI-74 UZDI-75 UZDI-76 UZDI-81 UZDI-82 UZDI-83 UZDI-84 Ajout de la classe UpdateRecipeRequest pour gérer la validation des requêtes de mise à jour des recettes.

---
+++ app/Http/Requests/Recipe/UpdateRecipeRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Recipe;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRecipeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title'             => 'sometimes|required|string|max:255',
+            'image'             => 'nullable|string',
+            'description'       => 'sometimes|required|string',
+            'preparation_time'  => 'sometimes|required|integer|min:1',
+            'cooking_time'      => 'sometimes|required|integer|min:1',
+            'servings'          => 'sometimes|required|integer|min:1',
+            'complexity'        => 'sometimes|required|in:Facile,Moyen,Difficile',
+            'ingredients'       => 'sometimes|required|string',
+            'instructions'      => 'sometimes|required|string',
+            'category_id'       => 'sometimes|required|exists:categories,id',
+            'tags'              => 'nullable|array',
+            'tags.*'            => 'exists:tags,id',
+        ];
+    }
+}
