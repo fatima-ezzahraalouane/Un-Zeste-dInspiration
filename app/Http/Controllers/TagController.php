4be5804ac2a016205994fc8658531e18551a4465
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-22 22:23:22 +0100

    UZDI-72 UZDI-73 UZDI-74 UZDI-75 UZDI-76 UZDI-81 UZDI-82 UZDI-83 UZDI-84   Ajout du contrôleur TagController pour gérer les opérations CRUD sur les tags.

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\Tag\StoreTagRequest;
+use App\Http\Requests\Tag\UpdateTagRequest;
+use App\Repositories\Tag\TagRepositoryInterface;
+
+class TagController extends Controller
+{
+    protected $tagRepo;
+
+    public function __construct(TagRepositoryInterface $tagRepo)
+    {
+        $this->tagRepo = $tagRepo;
+    }
+
+    public function index()
+    {
+        $tags = $this->tagRepo->all();
+        return view('tags.index', compact('tags'));
+    }
+
+    public function store(StoreTagRequest $request)
+    {
+        $this->tagRepo->store($request);
+        return redirect()->back()->with('success', 'Tag ajouté avec succès.');
+    }
+
+    public function update(UpdateTagRequest $request, $id)
+    {
+        $this->tagRepo->update($request, $id);
+        return redirect()->back()->with('success', 'Tag mis à jour avec succès.');
+    }
+
+    public function destroy($id)
+    {
+        $this->tagRepo->destroy($id);
+        return redirect()->back()->with('success', 'Tag supprimé avec succès.');
+    }
+}
