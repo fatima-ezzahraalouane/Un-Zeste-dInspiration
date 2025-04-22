c60fa3a9bfee5a94911f347fff4899ec9d674f35
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-22 17:28:31 +0100

    Ajout de la classe RegisterRequest pour gérer la validation des requêtes d'inscription des utilisateurs.

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name'            => 'required|string|max:100',
+            'last_name'             => 'required|string|max:100',
+            'email'                 => 'required|email|unique:users,email',
+            'password'              => 'required|string|min:8|confirmed',
+            'role'                  => 'required|in:Gourmand,Chef',
+        ];
+    }
+
+    public function messages(){
+        return [
+            'email.unique' => "cette adresse email est déjà utilisée"
+        ];
+    }
+}
