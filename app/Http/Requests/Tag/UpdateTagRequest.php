993e110f238799a20528f97f4f7a6963b7337263
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-23 17:11:07 +0100

    UZDI-50 UZDI-45 UZDI-51 UZDI-52 UZDI-53 UZDI-72 UZDI-121 UZDI-122 UZDI-123 UZDI-124 UZDI-125 Mise à jour de l'autorisation dans UpdateTagRequest pour restreindre l'accès aux utilisateurs ayant le rôle 'Admin'.

--- app/Http/Requests/Tag/UpdateTagRequest.php
+++ app/Http/Requests/Tag/UpdateTagRequest.php
@@ -11,7 +11,7 @@ class UpdateTagRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return auth()->check() && auth()->user()->role->name_user === 'Admin';
     }
 
     /**
