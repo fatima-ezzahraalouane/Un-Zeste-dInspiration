bd3b1f05d6b52d936eeb8806d30a0a4a7c54bc72
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-23 17:05:58 +0100

    UZDI-50 UZDI-45 UZDI-51 UZDI-52 UZDI-53 UZDI-72 UZDI-121 UZDI-122 UZDI-123 UZDI-124 UZDI-125 Mise à jour de l'autorisation dans UpdateRecipeRequest pour restreindre l'accès aux utilisateurs ayant le rôle 'Chef'.

--- app/Http/Requests/Recipe/UpdateRecipeRequest.php
+++ app/Http/Requests/Recipe/UpdateRecipeRequest.php
@@ -11,7 +11,7 @@ class UpdateRecipeRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return auth()->check() && auth()->user()->role->name_user === 'Chef';
     }
 
     /**
