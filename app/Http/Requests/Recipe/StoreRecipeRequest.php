1f78acdaca4ec3b05954ccdbbe6e944e60a22ed6
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-23 17:05:32 +0100

    UZDI-50 UZDI-45 UZDI-51 UZDI-52 UZDI-53 UZDI-72 UZDI-121 UZDI-122 UZDI-123 UZDI-124 UZDI-125 Mise à jour de l'autorisation dans StoreRecipeRequest pour restreindre l'accès aux utilisateurs ayant le rôle 'Chef'.

--- app/Http/Requests/Recipe/StoreRecipeRequest.php
+++ app/Http/Requests/Recipe/StoreRecipeRequest.php
@@ -11,7 +11,7 @@ class StoreRecipeRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return auth()->check() && auth()->user()->role->name_user === 'Chef';
     }
 
     /**
