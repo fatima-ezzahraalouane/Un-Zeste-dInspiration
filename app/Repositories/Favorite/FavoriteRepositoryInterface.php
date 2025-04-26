5448e1d9e01d067b7ed6e9fc3812b7b59884b59b
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-26 23:11:16 +0100

    UZDI-85 UZDI-72 UZDI-86 UZDI-87 UZDI-88 UZDI-89 UZDI-45 UZDI-50 UZDI-51 UZDI-52 UZDI-53 Mise à jour des types de requête dans FavoriteRepositoryInterface pour utiliser StoreFavoriteRequest et DeleteFavoriteRequest

--- app/Repositories/Favorite/FavoriteRepositoryInterface.php
+++ app/Repositories/Favorite/FavoriteRepositoryInterface.php
@@ -3,9 +3,11 @@
 namespace App\Repositories\Favorite;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\Favorite\DeleteFavoriteRequest;
+use App\Http\Requests\Favorite\StoreFavoriteRequest;
 
 interface FavoriteRepositoryInterface
 {
-    public function store(Request $request);
-    public function destroy(Request $request);
+    public function store(StoreFavoriteRequest $request);
+    public function destroy(DeleteFavoriteRequest $request);
 }
