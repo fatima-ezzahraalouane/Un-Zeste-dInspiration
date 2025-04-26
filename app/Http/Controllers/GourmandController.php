2f254a7b06cdc151860700e99ab07aeda97e57e5
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-26 18:43:51 +0100

    UZDI-85 UZDI-72 UZDI-86 UZDI-87 UZDI-88 UZDI-89 Ajout des statistiques des recettes, chefs et membres dans le contrôleur Gourmand.

--- app/Http/Controllers/GourmandController.php
+++ app/Http/Controllers/GourmandController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repositories\Recipe\RecipeRepositoryInterface;
+use App\Models\Recipe;
+use App\Models\Chef;
+use App\Models\User;
 
 class GourmandController extends Controller
 {
@@ -17,6 +20,16 @@ class GourmandController extends Controller
     public function index()
     {
         $recipes = $this->recipeRepo->getMostLikedRecipes(4);
-        return view('gourmand.accueil', compact('recipes'));
+
+        $stats = [
+            'recipes' => Recipe::count(),
+            'experiences' => 0,
+            'chefs' => Chef::count(),
+            'members' => User::whereHas('role', function ($q) {
+                $q->where('name_user', 'Gourmand');
+            })->count(),
+        ];
+
+        return view('gourmand.accueil', compact('recipes', 'stats'));
     }
 }
