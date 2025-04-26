3683ab95faf6519358beb2ff20b7ec005fb7bbae
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-26 18:39:34 +0100

    UZDI-85 UZDI-72 UZDI-86 UZDI-87 UZDI-88 UZDI-89 Ajout du contrôleur Gourmand avec la méthode d'affichage des recettes les plus appréciées.

--- app/Http/Controllers/GourmandController.php
+++ app/Http/Controllers/GourmandController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\Recipe\RecipeRepositoryInterface;
 
 class GourmandController extends Controller
 {
-    //
+    protected $recipeRepo;
+
+    public function __construct(RecipeRepositoryInterface $recipeRepo)
+    {
+        $this->recipeRepo = $recipeRepo;
+    }
+
+    public function index()
+    {
+        $recipes = $this->recipeRepo->getMostLikedRecipes(4);
+        return view('gourmand.accueil', compact('recipes'));
+    }
 }
