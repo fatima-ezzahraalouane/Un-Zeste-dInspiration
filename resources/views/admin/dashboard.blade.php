0394745cdc61e8f4a1d81085fbb78b2aed2aba61
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-27 18:59:48 +0100

    UZDI-10 UZDI-1 UZDI-11 UZDI-12 UZDI-13 UZDI-54 UZDI-58 UZDI-59 UZDI-60 UZDI-61 UZDI-62 UZDI-63 UZDI-71 modifier button de déconnexion

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -113,10 +113,13 @@
         <!-- Mobile Menu -->
         <div id="mobile-menu" class="hidden md:hidden bg-white shadow-md rounded-lg mt-2 mx-4 mb-4">
             <div class="flex flex-col px-4 py-2 space-y-2">
-                <a href="registre.html"
-                    class="shine-effect px-6 py-2 bg-brand-burgundy text-white rounded-full hover:shadow-lg transform hover:scale-105 transition-all duration-300">
-                    Déconnexion
-                </a>
+                <form action="{{ route('logout') }}" method="POST" class="inline">
+                    @csrf
+                    <button type="submit"
+                        class="shine-effect text-center px-6 py-2 bg-brand-burgundy text-white rounded-full hover:shadow-lg transform hover:scale-105 transition-all duration-300">
+                        Déconnexion
+                    </button>
+                </form>
             </div>
         </div>
     </nav>
