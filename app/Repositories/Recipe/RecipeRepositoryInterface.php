ddb1dc886e3e2c56a16748eb31363640709ce74c
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-27 14:22:05 +0100

    UZDI-85 UZDI-72 UZDI-86 UZDI-87 UZDI-88 UZDI-89 UZDI-45 UZDI-50 UZDI-51 UZDI-52 UZDI-53 UZDI-14 UZDI-24 UZDI-25 UZDI-26 UZDI-27 UZDI- 28 UZDI-29 UZDI-30 UZDI-90 UZDI-91 UZDI-92 UZDI-93 UZDI-94 UZDI-95 UZDI-96 Mise à jour des types de requêtes dans RecipeRepositoryInterface : utilisation de StoreRecipeRequest et UpdateRecipeRequest pour les méthodes store et update.

--- app/Repositories/Recipe/RecipeRepositoryInterface.php
+++ app/Repositories/Recipe/RecipeRepositoryInterface.php
@@ -2,14 +2,17 @@
 namespace App\Repositories\Recipe;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\Recipe\StoreRecipeRequest;
+use App\Http\Requests\Recipe\UpdateRecipeRequest;
 
 interface RecipeRepositoryInterface
 {
     public function index();
-    public function store(Request $request);
+    public function store(StoreRecipeRequest $request);
     public function show($id);
-    public function update(Request $request, $id);
+    public function update(UpdateRecipeRequest $request, $id);
     public function destroy($id);
 
     public function getMostLikedRecipes(int $limit);
+    public function search(Request $request);
 }
