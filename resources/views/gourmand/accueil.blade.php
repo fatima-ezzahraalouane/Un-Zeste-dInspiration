0c1a70efee90c32bfcb952660af1b1c1da8c6317
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-27 13:03:40 +0100

    UZDI-85 UZDI-72 UZDI-86 UZDI-87 UZDI-88 UZDI-89 UZDI-45 UZDI-50 UZDI-51 UZDI-52 UZDI-53 UZDI-14 UZDI-24 UZDI-25 UZDI-26 UZDI-27 UZDI-28 UZDI-29 UZDI-30 UZDI-90 UZDI-91 UZDI-92 UZDI-93 UZDI-94 UZDI-95 UZDI-96 Modification des routes de recherche et de navigation pour rediriger vers 'gourmand.recettes'

--- resources/views/gourmand/accueil.blade.php
+++ resources/views/gourmand/accueil.blade.php
@@ -117,7 +117,7 @@
                 <div class="relative max-w-2xl">
                     <input type="search" placeholder="Recherchez une recette ..."
                         class="w-full px-8 py-4 rounded-full bg-white/95 text-brand-dark placeholder-brand-gray focus:outline-none focus:ring-2 focus:ring-brand-burgundy/50">
-                    <a href="{{ route('gourmand.recette') }}"
+                    <a href="{{ route('gourmand.recettes') }}"
                         class="absolute right-4 top-1/2 -translate-y-1/2 bg-brand-burgundy text-white p-3 rounded-full hover:bg-brand-red transition-colors">
                         <i class="fas fa-search"></i>
                     </a>
@@ -195,7 +195,7 @@
             </div>
 
             <div class="text-center mt-8">
-                <a href="{{ route('gourmand.recette') }}" class="px-6 py-2 bg-brand-burgundy text-white rounded-full hover:bg-brand-red transition-colors">
+                <a href="{{ route('gourmand.recettes') }}" class="px-6 py-2 bg-brand-burgundy text-white rounded-full hover:bg-brand-red transition-colors">
                     Voir Plus
                 </a>
             </div>
