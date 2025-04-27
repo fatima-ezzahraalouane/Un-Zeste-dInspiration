88a9ee712a0404e70f8281229373b063d25d3fce
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-27 23:06:58 +0100

    UZDI-10 UZDI-1 UZDI-11 UZDI-12 UZDI-13 UZDI-54 UZDI-58 UZDI-59 UZDI-60 UZDI-61 UZDI-62 UZDI-63 UZDI-71 Ajout de la barre de navigation avec menu mobile et bouton de déconnexion.

---
+++ resources/views/partials/navbara.blade.php
@@ -0,0 +1,39 @@
+<nav class="fixed w-full bg-white/90 backdrop-blur-md shadow-sm z-50">
+    <div class="max-w-7xl mx-auto px-4">
+        <div class="flex justify-between items-center h-20">
+            <a href="{{ route('admin.dashboard') }}" class="flex items-center space-x-2">
+                <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-18 w-24">
+                <!-- <span class="playfair text-2xl font-bold text-brand-burgundy">
+                        Un Zeste d'Inspiration
+                    </span> -->
+            </a>
+
+            <div class="hidden md:flex items-center space-x-8">
+                <form action="{{ route('logout') }}" method="POST" class="inline">
+                    @csrf
+                    <button type="submit"
+                        class="shine-effect px-6 py-2 bg-brand-burgundy text-white rounded-full hover:shadow-lg transform hover:scale-105 transition-all duration-300">
+                        Déconnexion
+                    </button>
+                </form>
+            </div>
+
+            <button id="burger-menu" class="md:hidden text-brand-burgundy">
+                <i class="fas fa-bars text-2xl"></i>
+            </button>
+        </div>
+    </div>
+
+    <!-- Mobile Menu -->
+    <div id="mobile-menu" class="hidden md:hidden bg-white shadow-md rounded-lg mt-2 mx-4 mb-4">
+        <div class="flex flex-col px-4 py-2 space-y-2">
+            <form action="{{ route('logout') }}" method="POST" class="inline">
+                @csrf
+                <button type="submit"
+                    class="shine-effect text-center px-6 py-2 bg-brand-burgundy text-white rounded-full hover:shadow-lg transform hover:scale-105 transition-all duration-300">
+                    Déconnexion
+                </button>
+            </form>
+        </div>
+    </div>
+</nav>
