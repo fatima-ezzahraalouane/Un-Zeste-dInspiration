d1724e482adddaafdd560523ac03aafbfe772fc9
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-28 12:18:20 +0100

    UZDI-10 UZDI-1 UZDI-11 UZDI-12 UZDI-13 UZDI-54 UZDI-58 UZDI-59 UZDI-60 UZDI-61 UZDI-62 UZDI-63 UZDI-71 Ajout de la relation 'themes' dans le modèle 'Admin'

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -18,4 +18,9 @@ class Admin extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function themes()
+    {
+        return $this->hasMany(Theme::class);
+    }
 }
