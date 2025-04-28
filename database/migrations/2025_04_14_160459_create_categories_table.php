20b75ff72632def4ccfbf21e66719657499ce692
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-28 16:29:19 +0100

    UZDI-10 UZDI-1 UZDI-11 UZDI-12 UZDI-13 UZDI-54 UZDI-58 UZDI-59 UZDI-60 UZDI-61 UZDI-62 UZDI-63 UZDI-71 ajouter foreign key

--- database/migrations/2025_04_14_160459_create_categories_table.php
+++ database/migrations/2025_04_14_160459_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignId('admin_id')->constrained('admins')->onDelete('cascade');
             $table->timestamps();
         });
     }
