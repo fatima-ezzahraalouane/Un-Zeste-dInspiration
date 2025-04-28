41384ffffa66e6de17b686d5d6b192f066b74339
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-29 00:36:42 +0100

    UZDI-10 UZDI-1 UZDI-11 UZDI-12 UZDI-13 UZDI-54 UZDI-58 UZDI-59 UZDI-60 UZDI-61 UZDI-62 UZDI-63 UZDI-71 modifier les URL des images pour les expériences dans le seeder ExperienceSeeder

--- database/seeders/ExperienceSeeder.php
+++ database/seeders/ExperienceSeeder.php
@@ -15,7 +15,7 @@ class ExperienceSeeder extends Seeder
         $experiences = [
             [
                 'title' => 'Ma Première Tagine',
-                'image' => 'https://via.placeholder.com/400x300?text=Tagine',
+                'image' => 'https://www.essaouiratour.com/upload/tajine-boeuf-pruneaux-2.jpg',
                 'description' => 'Une expérience authentique de préparation de Tagine marocain.',
                 'theme_id' => 1,
                 'gourmand_id' => 1,
@@ -23,7 +23,7 @@ class ExperienceSeeder extends Seeder
             ],
             [
                 'title' => 'Secrets de la Pâtisserie Française',
-                'image' => 'https://via.placeholder.com/400x300?text=Patissierie',
+                'image' => 'https://jaime-patisser.com/cdn/shop/articles/67a50d88eb99d1738870152_1024x.webp?v=1738881226',
                 'description' => 'Découverte des bases de la pâtisserie classique.',
                 'theme_id' => 2,
                 'gourmand_id' => 1,
@@ -31,7 +31,7 @@ class ExperienceSeeder extends Seeder
             ],
             [
                 'title' => 'Voyage Street Food Bangkok',
-                'image' => 'https://via.placeholder.com/400x300?text=Street+Food',
+                'image' => 'https://i.f1g.fr/media/madame/1200x630_crop/sites/default/files/img/2016/02/bangkok-street-food-voyage-tourisme.jpg',
                 'description' => 'Découverte des saveurs thaïlandaises en mode street food.',
                 'theme_id' => 3,
                 'gourmand_id' => 1,
@@ -39,7 +39,7 @@ class ExperienceSeeder extends Seeder
             ],
             [
                 'title' => 'Cuisine Végétarienne Express',
-                'image' => 'https://via.placeholder.com/400x300?text=Vegetarienne',
+                'image' => 'https://img.over-blog-kiwi.com/0/94/02/18/20181018/ob_c03111_3031-chili-sin-carne.jpg',
                 'description' => 'Des plats végétariens faciles et rapides à préparer.',
                 'theme_id' => 4,
                 'gourmand_id' => 1,
@@ -47,7 +47,7 @@ class ExperienceSeeder extends Seeder
             ],
             [
                 'title' => 'Couscous Royal Maison',
-                'image' => 'https://via.placeholder.com/400x300?text=Couscous',
+                'image' => 'https://resize.elle.fr/original/var/plain_site/storage/images/elle-a-table/les-dossiers-de-la-redaction/news-de-la-redaction/recette-couscous-royal-maison-3971641/95716125-1-fre-FR/Le-couscous-royal-maison-l-une-des-recettes-preferees-des-Francais.jpg',
                 'description' => 'Préparation complète du couscous traditionnel.',
                 'theme_id' => 1,
                 'gourmand_id' => 1,
@@ -55,7 +55,7 @@ class ExperienceSeeder extends Seeder
             ],
             [
                 'title' => 'Baguette Artisanale',
-                'image' => 'https://via.placeholder.com/400x300?text=Baguette',
+                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1j4fo6OKV7l8IH8AsJ_wpqI1OqoWApu6PDw&s',
                 'description' => 'Secrets pour réussir une baguette comme un boulanger.',
                 'theme_id' => 2,
                 'gourmand_id' => 1,
@@ -63,7 +63,7 @@ class ExperienceSeeder extends Seeder
             ],
             [
                 'title' => 'Tacos Mexicains Maison',
-                'image' => 'https://via.placeholder.com/400x300?text=Tacos',
+                'image' => 'https://toupargel.fr/wp-content/uploads/2024/06/tacos-mexicain.jpeg',
                 'description' => 'La fabrication authentique des tacos maison.',
                 'theme_id' => 3,
                 'gourmand_id' => 1,
@@ -71,7 +71,7 @@ class ExperienceSeeder extends Seeder
             ],
             [
                 'title' => 'Cuisine Vegan Débutant',
-                'image' => 'https://via.placeholder.com/400x300?text=Vegan',
+                'image' => 'https://www.mavieencouleurs.fr/sites/default/files/styles/max_1300x1300/public/2022-10/header_vegetarien.png.webp?itok=yiXmNP3a',
                 'description' => 'Découverte de plats simples et savoureux sans produits animaux.',
                 'theme_id' => 4,
                 'gourmand_id' => 1,
@@ -79,7 +79,7 @@ class ExperienceSeeder extends Seeder
             ],
             [
                 'title' => 'Pastilla Sucrée Salée',
-                'image' => 'https://via.placeholder.com/400x300?text=Pastilla',
+                'image' => 'https://cache.marieclaire.fr/data/photo/w1200_h630_ci/1mo/recettes-pastilla-maison.jpg',
                 'description' => 'La fameuse Pastilla marocaine expliquée étape par étape.',
                 'theme_id' => 1,
                 'gourmand_id' => 1,
@@ -87,7 +87,7 @@ class ExperienceSeeder extends Seeder
             ],
             [
                 'title' => 'Atelier Chocolat Maison',
-                'image' => 'https://via.placeholder.com/400x300?text=Chocolat',
+                'image' => 'https://fribourg.ch/wp-content/uploads/2022/09/53939948244_1ccce3dd85_c.jpg',
                 'description' => 'Créer ses propres chocolats maison facilement.',
                 'theme_id' => 2,
                 'gourmand_id' => 1,
