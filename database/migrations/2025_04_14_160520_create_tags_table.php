65ef88e2541ee4dbc5b28b3c152b6402492a7f9e
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-28 18:45:06 +0100

    UZDI-10 UZDI-1 UZDI-11 UZDI-12 UZDI-13 UZDI-54 UZDI-58 UZDI-59 UZDI-60 UZDI-61 UZDI-62 UZDI-63 UZDI-71 supprimer la colonne admin_id de la table tags

--- database/migrations/2025_04_14_160520_create_tags_table.php
+++ database/migrations/2025_04_14_160520_create_tags_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('admin_id')->constrained('admins')->onDelete('cascade');
             $table->timestamps();
         });
     }
