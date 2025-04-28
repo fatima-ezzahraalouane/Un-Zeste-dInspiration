a2d81141e5842deea5306fef6d8a1576e7312739
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-29 00:37:18 +0100

    UZDI-10 UZDI-1 UZDI-11 UZDI-12 UZDI-13 UZDI-54 UZDI-58 UZDI-59 UZDI-60 UZDI-61 UZDI-62 UZDI-63 UZDI-71 modifier les URL des images dans le seeder ThemeSeeder pour les thèmes culinaires

--- database/seeders/ThemeSeeder.php
+++ database/seeders/ThemeSeeder.php
@@ -16,22 +16,22 @@ class ThemeSeeder extends Seeder
         $themes = [
             [
                 'name' => 'Cuisine Marocaine',
-                'image' => 'https://via.placeholder.com/400x300?text=Marocaine',
+                'image' => 'https://resize.elle.fr/article_1280/var/plain_site/storage/images/elle-a-table/les-dossiers-de-la-redaction/news-de-la-redaction/recettes-faciles-cuisine-marocaine-4173785/100676152-1-fre-FR/Cuisine-marocaine-5-recettes-pour-s-y-mettre.jpg',
                 'description' => 'Découvrez les saveurs traditionnelles du Maroc.'
             ],
             [
                 'name' => 'Pâtisserie Française',
-                'image' => 'https://via.placeholder.com/400x300?text=Patissierie',
+                'image' => 'https://www.centre-europeen-formation.fr/wp-content/uploads/2020/10/patisseries-en-france.jpg',
                 'description' => 'Savourez les douceurs de la pâtisserie française.'
             ],
             [
                 'name' => 'Street Food Internationale',
-                'image' => 'https://via.placeholder.com/400x300?text=Street+Food',
+                'image' => 'https://m.media-amazon.com/images/I/8189CrpqsTL._AC_UF1000,1000_QL80_.jpg',
                 'description' => 'Une aventure gustative autour du monde.'
             ],
             [
                 'name' => 'Cuisine Végétarienne',
-                'image' => 'https://via.placeholder.com/400x300?text=Vegetarienne',
+                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqErJ__EoSrkvcntiJJSQrrtzhZDLyaOw5Iw&s',
                 'description' => 'Des plats sains, gourmands et sans viande.'
             ],
         ];
