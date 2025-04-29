751ca46147bdbaa43c442f4355a396034956bfed
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-29 11:04:48 +0100

    UZDI-40 UZDI-31 UZDI-41 UZDI-42 UZDI-43 UZDI-44 UZDI-72 UZDI-103 UZDI-105 UZDI-106 UZDI-107 UZDI-108 UZDI-109 UZDI-110 I-72 UZDI-103 UZDI-105 UZDI-106 UZDI-107 UZDI-108 UZDI-109 UZDI-110 UZDI-111 UZDI-112 UZDI-113 UZDI-114 UZDI-115 UZDI-116 UZDI-117 UZDI-118 UZDI-119 UZDI-120 Ajout de l'interface ExperienceRepositoryInterface pour gérer les opérations liées aux expériences

---
+++ app/Repositories/Experience/ExperienceRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Experience;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\Experience\StoreExperienceRequest;
+use App\Http\Requests\Experience\UpdateExperienceRequest;
+use App\Models\Theme;
+
+interface ExperienceRepositoryInterface
+{
+    public function browse(Request $request, Theme $theme);
+    public function store(StoreExperienceRequest $request);
+    public function update(UpdateExperienceRequest $request, $id);
+    public function destroy($id);
+}
