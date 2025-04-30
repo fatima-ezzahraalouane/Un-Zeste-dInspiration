2dad290dd78128e96231f2395ce5739fde66988c
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-30 19:33:15 +0100

    UZDI-40 UZDI-31 UZDI-41 UZDI-42 UZDI-43 UZDI-44 UZDI-72 UZDI-103 UZDI-105 UZDI-106 UZDI-107 UZDI-108 UZDI-109 UZDI-110 I-72 UZDI-103 UZDI-105 UZDI-106 UZDI-107 UZDI-108 UZDI-109 UZDI-110 UZDI-111 UZDI-112 UZDI-113 UZDI-114 UZDI-115 UZDI-116 UZDI-117 UZDI-118 UZDI-119 UZDI-120 Modifier les types de requêtes dans CategoryRepositoryInterface pour utiliser StoreCategoryRequest et UpdateCategoryRequest

--- app/Repositories/Category/CategoryRepositoryInterface.php
+++ app/Repositories/Category/CategoryRepositoryInterface.php
@@ -2,13 +2,15 @@
 
 namespace App\Repositories\Category;
 
+use App\Http\Requests\Category\StoreCategoryRequest;
+use App\Http\Requests\Category\UpdateCategoryRequest;
 use Illuminate\Http\Request;
 
 interface CategoryRepositoryInterface
 {
     public function getAll();
-    public function store(Request $request);
-    public function update(Request $request, int $id);
+    public function store(StoreCategoryRequest $request);
+    public function update(UpdateCategoryRequest $request, int $id);
     public function delete(int $id);
     public function findById(int $id);
 }
