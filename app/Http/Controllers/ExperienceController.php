5d2c2cff36767bb949de25105fd57d531154a725
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-30 11:22:34 +0100

    UZDI-40 UZDI-31 UZDI-41 UZDI-42 UZDI-43 UZDI-44 UZDI-72 UZDI-103 UZDI-105 UZDI-106 UZDI-107 UZDI-108 UZDI-109 UZDI-110 I-72 UZDI-103 UZDI-105 UZDI-106 UZDI-107 UZDI-108 UZDI-109 UZDI-110 UZDI-111 UZDI-112 UZDI-113 UZDI-114 UZDI-115 UZDI-116 UZDI-117 UZDI-118 UZDI-119 UZDI-120 débuger l'erreur de l'ajoute de nouvelle experience

--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -23,13 +23,16 @@ class ExperienceController extends Controller
     {
         $experiences = $this->experienceRepo->browse($request, $theme);
 
-        return view('gourmand.experiences', compact('theme', 'experiences'));
+        $themes = $this->themeRepo->getAll();
+
+        return view('gourmand.experiences', compact('theme', 'experiences', 'themes'));
     }
 
     public function store(StoreExperienceRequest $request)
     {
+        // dd($request->all());
         $this->experienceRepo->store($request);
-
-        return redirect()->back()->with('success', 'Votre expérience a été soumise pour validation.');
+    
+        return redirect()->back()->with('success', 'Votre expérience a été soumise pour validation et sera approuvée par un administrateur.');
     }
 }
