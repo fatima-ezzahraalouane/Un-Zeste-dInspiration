dfb4e2797e0613302cc1281e7044c116d9d8fcab
fatima-ezzahraalouane <falouane38@gmail.com>
2025-05-01 17:38:25 +0100

    Améliorer la suppression de l'alerte de succès avec une transition d'opacité

--- resources/views/gourmand/mesfavoris.blade.php
+++ resources/views/gourmand/mesfavoris.blade.php
@@ -236,11 +236,12 @@
         setTimeout(() => {
             const alert = document.getElementById('success-alert');
             if (alert) {
-                alert.remove();
+                alert.style.transition = 'opacity 0.5s ease';
+                alert.style.opacity = '0';
+                setTimeout(() => alert.remove(), 500);
             }
         }, 2000);
     </script>
-
 </body>
 
 </html>
