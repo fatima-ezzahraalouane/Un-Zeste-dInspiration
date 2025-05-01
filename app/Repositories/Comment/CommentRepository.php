2c11cb449640c3130006e24595869cca5fedc921
fatima-ezzahraalouane <falouane38@gmail.com>
2025-05-01 20:37:39 +0100

    Ajouter le dépôt CommentRepository pour gérer les opérations CRUD sur les commentaires

---
+++ app/Repositories/Comment/CommentRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Repositories\Comment;
+
+use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Comment\StoreCommentRequest;
+use App\Http\Requests\Comment\UpdateCommentRequest;
+
+class CommentRepository implements CommentRepositoryInterface
+{
+    public function getAll()
+    {
+        return Comment::with('commentable', 'gourmand.user')->latest()->get();
+    }
+
+    public function store(StoreCommentRequest $request)
+    {
+        return Comment::create([
+            'contenu' => $request->contenu,
+            'commentable_id' => $request->commentable_id,
+            'commentable_type' => $request->commentable_type,
+            'gourmand_id' => Auth::user()->gourmand->id,
+        ]);
+    }
+
+    public function update(UpdateCommentRequest $request, $id)
+    {
+        $comment = Comment::findOrFail($id);
+        if ($comment->gourmand_id !== Auth::user()->gourmand->id) {
+            abort(403);
+        }
+        $comment->update(['contenu' => $request->contenu]);
+        return $comment;
+    }
+
+    public function destroy($id)
+    {
+        $comment = Comment::findOrFail($id);
+        if ($comment->gourmand_id !== Auth::user()->gourmand->id) {
+            abort(403);
+        }
+        $comment->delete();
+        return true;
+    }
+}
