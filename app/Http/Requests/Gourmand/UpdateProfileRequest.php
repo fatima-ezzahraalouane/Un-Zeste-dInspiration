412299183b8628f65478d80002094ceb2ad55286
fatima-ezzahraalouane <falouane38@gmail.com>
2025-05-02 00:49:18 +0100

    Ajouter la classe UpdateProfileRequest avec les règles de validation pour la mise à jour du profil utilisateur

---
+++ app/Http/Requests/Gourmand/UpdateProfileRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Gourmand;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check() && auth()->user()->role->name_user === 'Gourmand';
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name'  => 'required|string|max:255',
+            'email'      => 'required|email|max:255',
+            'adresse'    => 'nullable|string|max:255',
+            'biographie' => 'nullable|string|max:1000',
+        ];
+    }
+}
