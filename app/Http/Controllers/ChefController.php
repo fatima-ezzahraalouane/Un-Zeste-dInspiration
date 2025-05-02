f1cf91fa55e086aad1ee138be086f5ffe21386cf
fatima-ezzahraalouane <falouane38@gmail.com>
2025-05-02 15:14:17 +0100

    UZDI-54 UZDI-55 UZDI-56 UZDI-57 UZDI-14 UZDI-15 UZDI-16 UZDI-17 UZDI-18 UZDI-19 UZDI-20 UZDI-21 UZDI-23 Ajouter la méthode de profil au contrôleur Chef pour gérer l'affichage du tableau de bord

--- app/Http/Controllers/ChefController.php
+++ app/Http/Controllers/ChefController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChefController extends Controller
 {
-    //
+    public function profile()
+    {
+        $chef = Auth::user()->chef;
+
+        if (!$chef) {
+            abort(403, 'Accès non autorisé.');
+        }
+        
+        return view('chef.dashboard', compact('chef'));
+    }
 }
