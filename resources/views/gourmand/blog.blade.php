d18c554cb849a50e88af5bc509d84fe552d7ae6a
fatima-ezzahraalouane <falouane38@gmail.com>
2025-05-05 23:54:54 +0100

    Réduit la limite de caractères de la description des thèmes à 50 dans le blog.

--- resources/views/gourmand/blog.blade.php
+++ resources/views/gourmand/blog.blade.php
@@ -67,7 +67,7 @@
                     <img src="{{ $theme->image }}" alt="{{ $theme->name }}" class="w-full h-48 object-cover">
                     <div class="p-6">
                         <h3 class="text-xl font-semibold mb-3 text-brand-burgundy">{{ $theme->name }}</h3>
-                        <p class="text-brand-gray mb-4">{{ Str::limit($theme->description, 80) }}</p>
+                        <p class="text-brand-gray mb-4">{{ Str::limit($theme->description, 50) }}</p>
                         <a href="{{ route('gourmand.experiences', ['theme' => $theme->id]) }}"
                             class="block text-center px-4 py-2 bg-brand-burgundy text-white rounded-full hover:bg-brand-red transition-colors">
                             Explorer
